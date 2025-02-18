90ad8ec94962cc34a0b37eaff3df9215b31c9f75
ozncncyr <ozncncyr@outlook.com>
2025-02-18 23:27:01 +0300

    Implement form submission handling with validation and logging in task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,17 @@
 // Task-4
 
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    const { email, password } = e.currentTarget.elements;
+    if (email.value === "" || password.value === "") {
+        return alert("All form fields must be filled in!");
+    }
+    const formData = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(formData);
+    form.reset();
+});

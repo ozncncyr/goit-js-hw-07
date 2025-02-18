5d0c833f5478f1b16c76afed05ca393f2a2378fd
ozncncyr <ozncncyr@outlook.com>
2025-02-19 00:00:21 +0300

    Implement box creation and destruction functionality with random color generation

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,40 @@
+// Task-6
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = [];
+
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  return boxes;
+};
+
+const destroyBoxes = () => {
+  boxes.forEach(box => {
+    box.remove();
+  });
+  boxes.length = 0;
+};
+
+createBtn.addEventListener("click", () => {
+  destroyBoxes();
+  const amount = input.value;
+  createBoxes(amount).forEach(box => {
+    document.querySelector("#boxes").append(box);
+  });
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
